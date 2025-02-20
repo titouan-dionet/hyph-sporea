c5c5044a5e79b0cb8d6a2ff4a74c08621f31e010
Titouan Dionet <titouan.dionet@univ-lorraine.fr>
2025-02-20 14:17:27 +0100

    Update tiff_to_jpeg.py

--- Python/tiff_to_jpeg.py
+++ Python/tiff_to_jpeg.py
@@ -10,8 +10,8 @@ Created on Sun Feb 16 15:45:20 2025
 import os
 from PIL import Image
 
-# import tkinter as tk
-# from tkinter import ttk
+import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 
 # from Python.get_user_input_function import get_user_input
@@ -20,18 +20,18 @@ from Python.tiff_to_jpeg_function import convert_tif_to_jpeg
 #%% Initialisation de tkinter
 # Cette initialisation est nécessaire pour le bon déroulement du programme
 
-# init = tk.Tk()
-# init.title("Initialisation")
-# init.resizable(0,0)
-# init.wm_attributes("-topmost", 1)
+init = tk.Tk()
+init.title("Initialisation")
+init.resizable(0,0)
+init.wm_attributes("-topmost", 1)
 
-# text_label = tk.Label(init, text="Initialisation of the program.\nPlease, click OK to continue.", padx=10, pady=10)
-# text_label.pack()
+text_label = tk.Label(init, text="Initialisation of the program.\nPlease, click OK to continue.", padx=10, pady=10)
+text_label.pack()
 
-# close_button = ttk.Button(init, text="OK", command=init.destroy)
-# close_button.pack()
+close_button = ttk.Button(init, text="OK", command=init.destroy)
+close_button.pack()
 
-# init.mainloop()
+init.mainloop()
 
 #%% Choix des dossier d'entrée et de sortie
 

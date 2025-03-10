25cd0ae34f8a92a23735db707e7e2278af7330d6
Titouan Dionet <titouan.dionet@univ-lorraine.fr>
2025-03-10 16:34:21 +0100

    Create gui_main.py

---
+++ gui_main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+HYPH-SPOREA: Lancement de l'interface graphique principale
+"""
+
+from Python.gui.main_gui import run_gui
+
+if __name__ == "__main__":
+    run_gui()
